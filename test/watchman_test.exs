1578984b8621157bd6e68bc30a5c52067e71bd5f
Igor Šarčević <igor@renderedtext.com>
2016-10-27 23:30:50 +0200

    Write tests for annotation based benchmark

--- test/watchman_test.exs
+++ test/watchman_test.exs
@@ -1,6 +1,5 @@
 defmodule WatchmanTest do
   use ExUnit.Case
-  use Watchman.Benchmark
   use Watchman.Count
 
   @test_port 8125
@@ -9,16 +8,6 @@ defmodule WatchmanTest do
     TestUDPServer.start_link(port: @test_port)
 
     :ok
-  end
-
-  @benchmark(key: :auto)
-  def test_function1 do
-    :timer.sleep(1000)
-  end
-
-  @benchmark(key: "goddammit.charlie")
-  def test_function2 do
-    :timer.sleep(500)
   end
 
   @count(key: :auto)
@@ -68,22 +57,6 @@ defmodule WatchmanTest do
     :timer.sleep(500)
 
     assert TestUDPServer.last_message =~ ~r/watchman.test.sleep.duration:5\d\d|ms/
-  end
-
-  test "benchmark annotation auto key test" do
-    test_function1
-
-    :timer.sleep(500)
-
-    assert TestUDPServer.last_message =~ ~r/watchman.test.watchman_test.test_function1:10\d\d|ms/
-  end
-
-  test "benchmark annotation manual key test" do
-    test_function2
-
-    :timer.sleep(500)
-
-    assert TestUDPServer.last_message =~ ~r/watchman.test.goddammit.charlie:5\d\d|ms/
   end
 
   test "count annotation auto key test" do
